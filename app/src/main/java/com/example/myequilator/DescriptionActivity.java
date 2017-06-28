461fe47448af331a2aa6246e0894abf50e765c32
Oleksandr Levenko <Vilagra@mail.ru>
2017-06-28 16:35:49 +0300

    added description

--- app/src/main/java/com/example/myequilator/DescriptionActivity.java
+++ app/src/main/java/com/example/myequilator/DescriptionActivity.java
@@ -5,25 +5,22 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.method.LinkMovementMethod;
 import android.view.View;
+import android.widget.TextView;
 
 public class DescriptionActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_description);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.content_description);
+        TextView textView = (TextView) findViewById(R.id.credit_text);
+        TextView textView2 = (TextView) findViewById(R.id.credit_text2);
+        textView.setMovementMethod(LinkMovementMethod.getInstance());
+        textView2.setMovementMethod(LinkMovementMethod.getInstance());
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
     }
 
 }
